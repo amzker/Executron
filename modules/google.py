647dae20bd2979adb2dc587f03a1e57c08fe4d44
Dr.Amzker <amzker@protonmail.com>
2023-09-26 19:15:35 +0530

    optimized google.py code to mimic model structure

--- modules/google.py
+++ modules/google.py
@@ -1,36 +1,25 @@
-import os
 import webbrowser
 import requests
 import urllib.parse as parser
-from urllib.request import urlopen
-import datetime
-import tkinter
-from .notification import notification 
-
-
 import configparser
+from .notification import notification
+
 config = configparser.ConfigParser()
 config.read("config.paper")
 
-google_qurl = config.get("Default","google_qurl")
+google_qurl = config.get("Default", "google_qurl")
 gimage_reverse_url = config.get("Default", "gimage_reverse_url")
-screenshot_dir = config.get("Default","screenshot_dir") 
 
-def google(term):
-    if "clipboard" in term:
-        pass
-    else:
-        term = term.replace("google", "")
-        search = parser.quote(term)
-        webbrowser.open(google_qurl+search)
+def google_search(term):
+    term = term.replace("google", "")
+    search = parser.quote(term)
+    webbrowser.open(google_qurl + search)
 
-
-        
-def searchimg(filePath):
-    searchUrl = gimage_reverse_url
+def search_image(filePath):
+    search_url = gimage_reverse_url
     multipart = {'encoded_image': (filePath, open(filePath, 'rb')), 'image_content': ''}
-    response = requests.post(searchUrl, files=multipart, allow_redirects=False)
+    response = requests.post(search_url, files=multipart, allow_redirects=False)
     text = str(response.content)
-    text = text.split("URL=", 1)[1].replace("/></head></html>\'","")
+    text = text.split("URL=", 1)[1].replace("/></head></html>\'", "")
     webbrowser.open(text)
     notification("Check browser")
