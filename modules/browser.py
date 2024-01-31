ff33b03cd9e458bab3b9fe232a5f2d1057daa186
Dr.Amzker <amzker@protonmail.com>
2024-01-31 11:14:52 +0530

    init

--- modules/browser.py
+++ modules/browser.py
@@ -15,11 +15,27 @@ def google_search(term):
     search = parser.quote(term)
     webbrowser.open(google_qurl + search)
 
+import time
+
 def search_image(filePath):
-    search_url = gimage_reverse_url
+    print(filePath)
+    searchUrl = gimage_reverse_url
     multipart = {'encoded_image': (filePath, open(filePath, 'rb')), 'image_content': ''}
-    response = requests.post(search_url, files=multipart, allow_redirects=False)
-    text = str(response.content)
-    text = text.split("URL=", 1)[1].replace("/></head></html>\'", "")
-    webbrowser.open(text)
-    notification("Check browser")
+    response = requests.post(searchUrl, files=multipart, allow_redirects=True)
+    print(response.status_code)
+    #print(response.content)
+    if response.status_code == 502:
+        # Handle the 502 server error gracefully
+        print("Received a 502 (Server Error). Will retry in 30 seconds.")
+        time.sleep(30)
+        search_image(filePath)  # Retry the request
+    elif b"url=" in response.content:
+        # Handle success case
+        text = response.content.decode('utf-8')
+        print(text)
+        text = text.split("URL=", 1)[1].replace("/></head></html>\'", "")
+        webbrowser.open(text)
+        notification("Check the browser")
+    else:
+        # Handle other cases or notify the user
+        notification("Failed to find the image URL in the response")
--- paper.py
+++ paper.py
@@ -1,29 +1,24 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[26]:
-
-
-import configparser
-import json 
-config = configparser.ConfigParser()
-config.read("config.paper")
-tr_model_path = config.get('Default','tr_model_path')
-username = config.get('Default','username')
-asistant_name = config.get('Default','asistant_name')
-synames = json.loads(config.get('Default','synames'))
-screenshot_dir = config.get('Default','screenshot_dir')
-modules_dir = config.get('Default','modules_dir')
-
 import os
 import sys
 import importlib
-
+import argparse
 from vosk import Model, KaldiRecognizer
 import queue
 import sounddevice as sd
 import signal
-
+import json
+import configparser
+config = configparser.ConfigParser()
+config.read("config.paper")
+tr_model_path = config.get('Default', 'tr_model_path')
+username = config.get('Default', 'username')
+asistant_name = config.get('Default', 'asistant_name')
+synames = json.loads(config.get('Default', 'synames'))
+screenshot_dir = config.get('Default', 'screenshot_dir')
+modules_dir = config.get('Default', 'modules_dir')
 
 from context_recognition.predict import classify
 notification = importlib.import_module(f'{modules_dir}.notification').notification
@@ -31,36 +26,22 @@ notification = importlib.import_module(f'{modules_dir}.notification').notificati
 import warnings
 warnings.filterwarnings("ignore")
 
-
-device_info = sd.query_devices(sd.default.device[0], 'input')
-samplerate = int(device_info['default_samplerate'])
-
-q = queue.Queue()
-
-running = True
-
-
 def signal_handler(signal, frame):
     global running
     running = False
-
-signal.signal(signal.SIGINT, signal_handler)
-
-
 
 def recordCallback(indata, frames, time, status):
     if status:
         print(status, file=sys.stderr)
     q.put(bytes(indata))
 
-model = Model(tr_model_path)
-recognizer = KaldiRecognizer(model, samplerate)
-recognizer.SetWords(False)
+def process_voice_input(q, model, synames, username):
+    model = Model(model)
+    recognizer = KaldiRecognizer(model, samplerate)
+    recognizer.SetWords(False)
 
-try:
     with sd.RawInputStream(dtype='int16',
                            channels=1,
-
                            callback=recordCallback):
         while running:
             data = q.get()
@@ -72,11 +53,10 @@ try:
                 if recognized_text:
                     print(recognizerResult)
 
-
                     for asname in synames:
                         if asname in recognized_text:
                             recognized_text = recognized_text.replace(asname,"")
-                            notification(username+ ": " + recognized_text)
+                            notification(username + ": " + recognized_text)
                             action = classify(recognized_text.lower())
                             try:
                                 print(action)
@@ -90,13 +70,10 @@ try:
                                     command_func = getattr(module, func_name)
 
                                     if previous_output:
-                                        # Use the previous function's output
                                         previous_output = command_func(previous_output)
                                     else:
-                                        # Execute the action without previous output
                                         previous_output = command_func()
 
-                                # Store or use the final output as needed
                                 final_output = previous_output
 
                             except Exception as e:
@@ -107,13 +84,40 @@ try:
                     print("No input sound")
 
 
-except KeyboardInterrupt:
-    print('Finished Recording')
-except Exception as e:
-    print(str(e))
+parser = argparse.ArgumentParser()
+parser.add_argument("--text", action="store_true", help="Use text input instead of voice")
+args = parser.parse_args()
 
 
 
+device_info = sd.query_devices(sd.default.device[0], 'input')
+samplerate = int(device_info['default_samplerate'])
 
+q = queue.Queue()
+running = True
 
+if args.text:
+    while True:
+        try: 
+        # Use text input
+            text_input = input("Enter your text command: ")
+            action = classify(text_input.lower())
+            print(action)
+            actions_to_execute = action.split(',')
+            previous_output = None
 
+            for action in actions_to_execute:
+                module_name, func_name = action.split('.')
+                module_path = f'{modules_dir}.{module_name}'
+                module = importlib.import_module(module_path)
+                command_func = getattr(module, func_name)
+
+                if previous_output:
+                    previous_output = command_func(previous_output)
+                else:
+                    previous_output = command_func()
+        except Exception as e:
+            notification(str(e))
+            print("Error:", str(e))
+else:
+    process_voice_input(q, tr_model_path, synames, username)
